deee40b3ed371434d4917fda3ce649220438e582
robgolding63 <robgolding63@aec7fc74-b8b6-11de-97ce-e783fd196cab>
2010-03-24 14:34:03 +0000

    Changed search index to RealTimeSearchIndex

--- democonf/rooms/search_indexes.py
+++ democonf/rooms/search_indexes.py
@@ -1,6 +1,15 @@
+import datetime
+
 from haystack import site
+from haystack.indexes import *
 
 from models import Room
 
-site.register(Room)
+class RoomIndex(RealTimeSearchIndex):
+	text = CharField(document=True, use_template=True)
+	
+	def get_queryset(self):
+		"""Used when the entire index for model is updated."""
+		return Room.objects.all()
 
+site.register(Room, RoomIndex)
